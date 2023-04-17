46815bd50b2962e049fb6abc646e5e0b9fee74fb
mtrupert <89813721+mtrupert@users.noreply.github.com>
2023-04-17 17:09:46 -0500

    Delete creds.py

--- REST API dev/creds.py
+++
@@ -1,6 +0,0 @@
-# You need to give your credentials here
-class Creds:
-    connectionstring = 'cis3368spring.c3komviprvkg.us-east-1.rds.amazonaws.com'
-    username = 'MTRupert'
-    passwd = 'Sal00nD00r'
-    dataBase = 'cis3368springdb'
